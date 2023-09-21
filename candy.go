92ec8a9ed354e3b62c1c761918b0c52985c02837
y.ashenov <YerzhanAshenov@LiveNation.com>
2023-09-21 12:49:54 +0600

    daily: int to roman

--- candy.go
+++ candy.go
@@ -1,1 +1,31 @@
 package main
+
+func Candy(ratings []int) int {
+	n := len(ratings)
+
+	candies := make([]int, n)
+	for i := range candies {
+		candies[i] = 1
+	}
+
+	for i := 1; i < len(ratings); i++ {
+		if ratings[i] > ratings[i-1] {
+			candies[i] = candies[i-1] + 1
+		}
+	}
+
+	for j := n - 2; j >= 0; j-- {
+		if ratings[j] > ratings[j+1] {
+			if candies[j] <= candies[j+1] {
+				candies[j] = candies[j+1] + 1
+			}
+		}
+	}
+
+	totalCandies := 0
+	for _, candy := range candies {
+		totalCandies += candy
+	}
+
+	return totalCandies
+}
--- candy_test.go
+++ candy_test.go
@@ -1,5 +1,13 @@
 package main
 
-func Candy(ratings []int) int {
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
 
+func TestCandy(t *testing.T) {
+	//assert.Equal(t, Candy([]int{1, 0, 2}), 5)
+	//assert.Equal(t, Candy([]int{1, 2, 2}), 4)
+	//assert.Equal(t, Candy([]int{1, 3, 2, 2, 1}), 7)
+	assert.Equal(t, Candy([]int{1, 2, 87, 87, 87, 2, 1}), 13)
 }
